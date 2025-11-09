ba9e590276eec36b71f15213fa32af708a60654a
Woopsyyy <yoshiru28@gmail.com>
2025-11-09 18:01:58 +0800

    fix the issue of user

--- public/user_management.php
+++ public/user_management.php
@@ -17,6 +17,8 @@ $conn = Database::getInstance()->getConnection();
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
     <link rel="stylesheet" href="css/home.css" />
     <link rel="stylesheet" href="css/ui_tweaks.css" />
     <link rel="stylesheet" href="css/admin_dashboard.css" />
